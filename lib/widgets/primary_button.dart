e4d837c2731e800ade5d678e6cec3970429226b6
Shadrack Ongera <shadrack.ongera@ronforddigital.com>
2024-01-04 00:03:33 +0300

    primary button edit

--- lib/widgets/primary_button.dart
+++ lib/widgets/primary_button.dart
@@ -19,13 +19,21 @@ class _PrimaryButtonState extends State<PrimaryButton> {
     return GestureDetector(
       onTap: widget.onTap,
       child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 8,
+        ),
         width: double.infinity,
         decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(8)
         ),
         child: Center(
           child: Text(
             widget.text,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.background,
+              fontWeight: FontWeight.w500
+            ),
           ),
         ),
       ),
